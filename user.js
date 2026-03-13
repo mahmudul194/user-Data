85a191518f32368705ebd96ae126d6914badf555
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-13 21:33:45 +0600

    added the displayuser function

--- user.js
+++ user.js
@@ -1,4 +1,13 @@
 const LoadUser = () =>{
     const Url = "https://jsonplaceholder.typicode.com/users"
-    fetch(Url)
+    fetch(Url).then(res => res.json()).then(json => displayUser(json))
 }
+
+const displayUser = users =>{
+    users.forEach(user =>{
+        const userContainer = document.getElementById('user-container')
+        userContainer.innerHTML = ' '
+    })
+}
+
+LoadUser();
