9c379156f49bb4b9d96682b850c0aa8bb75e6923
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-13 22:01:42 +0600

    added user details in the display

--- user.js
+++ user.js
@@ -1,13 +1,35 @@
-const LoadUser = () =>{
-    const Url = "https://jsonplaceholder.typicode.com/users"
-    fetch(Url).then(res => res.json()).then(json => displayUser(json))
-}
+const LoadUser = () => {
+  const Url = "https://jsonplaceholder.typicode.com/users";
+  fetch(Url)
+    .then((res) => res.json())
+    .then((json) => displayUser(json));
+};
 
-const displayUser = users =>{
-    users.forEach(user =>{
-        const userContainer = document.getElementById('user-container')
-        userContainer.innerHTML = ' '
-    })
-}
+const displayUser = (users) => {
+  const userContainer = document.getElementById("user-container");
+  // userContainer.innerHTML = ' '
+  users.forEach((user) => {
+    const userCard = document.createElement("div");
+    userCard.innerHTML = `
+        <div class="user-card grid border rounded-md w-[500px] shadow-2xl">
+        <h1 class="text-center font-bold text-2xl"> ${user.name} </h1>
+        <p> ${user.address.street} </p>
+        <p> ${user.address.suite} </p>
+        <p> ${user.address.city} </p>
+        <p> ${user.address.zipcode} </p>
+        <p> ${user.address.geo.lat} </p>
+        <p> ${user.address.geo.lng} </p>
+        <p> ${user.phone} </p>
+        <p> ${user.website} </p>
+        <p>  </p>
+        <p>  </p>
+        <p>  </p>
+        <p>  </p>
+        </div>
+        
+        `;
+    userContainer.appendChild(userCard);
+  });
+};
 
 LoadUser();
