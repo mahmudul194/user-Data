1f77d346478a5779f10728666cf57a3eb0d93019
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-13 22:21:33 +0600

    added the user details

--- user.js
+++ user.js
@@ -7,23 +7,25 @@ const LoadUser = () => {
 
 const displayUser = (users) => {
   const userContainer = document.getElementById("user-container");
-  // userContainer.innerHTML = ' '
+  userContainer.innerHTML = ''
   users.forEach((user) => {
     const userCard = document.createElement("div");
     userCard.innerHTML = `
         <div class="user-card grid border rounded-md w-[500px] shadow-2xl">
         <h1 class="text-center font-bold text-2xl"> ${user.name} </h1>
-        <p> ${user.address.street} </p>
-        <p> ${user.address.suite} </p>
-        <p> ${user.address.city} </p>
-        <p> ${user.address.zipcode} </p>
-        <p> ${user.address.geo.lat} </p>
-        <p> ${user.address.geo.lng} </p>
-        <p> ${user.phone} </p>
-        <p> ${user.website} </p>
-        <p>  </p>
-        <p>  </p>
-        <p>  </p>
+        <p> <span class='font-bold' >Username :</span> ${user.username} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.email} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.address.street} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.address.suite} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.address.city} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.address.zipcode} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.address.geo.lat} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.address.geo.lng} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.phone} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.website} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.company.name} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.company.catchPhrase} </p>
+        <p>  <span class='font-bold' >Username :</span> ${user.company.bs} </p>
         <p>  </p>
         </div>
         
