e4d2ffdd8b5e6475c2957cdfdb84bc01eeeb436d
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-13 23:03:32 +0600

    finished the display user logic

--- user.js
+++ user.js
@@ -11,22 +11,18 @@ const displayUser = (users) => {
   users.forEach((user) => {
     const userCard = document.createElement("div");
     userCard.innerHTML = `
-        <div class="user-card grid border rounded-md w-[500px] shadow-2xl">
+        <div class="user-card border rounded-md shadow-2xl">
         <h1 class="text-center font-bold text-2xl"> ${user.name} </h1>
         <p> <span class='font-bold' >Username :</span> ${user.username} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.email} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.address.street} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.address.suite} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.address.city} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.address.zipcode} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.address.geo.lat} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.address.geo.lng} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.phone} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.website} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.company.name} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.company.catchPhrase} </p>
-        <p>  <span class='font-bold' >Username :</span> ${user.company.bs} </p>
-        <p>  </p>
+        <p>  <span class='font-bold' >Email :</span> ${user.email} </p>
+        <p>  <span class='font-bold' >Address :</span> ${user.address.street}, ${user.address.suite}, ${user.address.city} </p>
+        <p>  <span class='font-bold' >Zip Code :</span> ${user.address.zipcode} </p>
+        <p>  <span class='font-bold' >Geo :</span> ${user.address.geo.lat}/${user.address.geo.lng} </p>
+        <p>  <span class='font-bold' >Phone No. :</span> ${user.phone} </p>
+        <p>  <span class='font-bold' >Website :</span> ${user.website} </p>
+        <p>  <span class='font-bold' >Company Name :</span> ${user.company.name} </p>
+        <p>  <span class='font-bold' >Catch Phrase :</span> ${user.company.catchPhrase} </p>
+        <p>  <span class='font-bold' >Base Line :</span> ${user.company.bs} </p>
         </div>
         
         `;
